3ddb73c4afe94228028a913a67ef0264aa2c2939
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-04 16:37:04 +0300

    added layers and models

---
+++ Bank/Bank.DAL/Entities/CreditCardEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.DAL.Entities
+{
+    public class CreditCard
+    {
+        public int Id { get; set; }
+        public int CardNumber { get; set; }
+        public int CVV { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerSecondName { get; set; }
+    }
+}
