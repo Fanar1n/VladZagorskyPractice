c4960eeb350f440039d8bca46a8ed1cbe12b049a
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-07 18:02:24 +0300

    added FluentValidation

---
+++ Bank/Bank/Validator/CreditCardValidator.cs
@@ -0,0 +1,17 @@
+﻿using Bank.Models;
+using FluentValidation;
+
+namespace Bank.Validator
+{
+    public class CreditCardValidator : AbstractValidator<CreditCardViewModel>
+    {
+        public CreditCardValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.CardNumber).InclusiveBetween(1, 99999);
+            RuleFor(x => x.CVV).InclusiveBetween(1, 999);
+            RuleFor(x => x.OwnerFirstName).Length(1,30);
+            RuleFor(x => x.OwnerSecondName).Length(1,30);
+        }
+    }
+}
