a589e43ece38d85124eae7f117f74f1c8cfc5d08
Fanar1n <95366750+Fanar1n@users.noreply.github.com>
2022-07-08 11:44:33 +0300

    add new entity and migration (#16)

--- Bank/Bank.DAL/EF/ApplicationContext.cs
+++ Bank/Bank.DAL/EF/ApplicationContext.cs
@@ -1,13 +1,15 @@
-﻿using Bank.DAL.Entities;
+﻿using System.Dynamic;
+using Bank.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bank.DAL.EF
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<CreditCardEntity> CreditCard => Set<CreditCardEntity>();
+        public DbSet<CreditCardEntity> CreditCard { get; set; }
+        public DbSet<ClientEntity> Client { get; set; }
 
-        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { Database.EnsureCreated(); }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { Database.Migrate(); }
 
     }
 }
---
+++ Bank/Bank.DAL/Entities/ClientEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Bank.DAL.Entities
+{
+    public class ClientEntity
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
