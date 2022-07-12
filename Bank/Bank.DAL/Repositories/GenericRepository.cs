4c8e027b699fab1b95192ce1cbb104560fb8e3b5
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-12 11:35:26 +0300

    add generic repository

---
+++ Bank/Bank.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Bank.DAL.EF;
+using Bank.DAL.Entities;
+using Bank.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bank.DAL.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationContext _db;
+
+        protected readonly DbSet<TEntity> _dbSet;
+        public GenericRepository(ApplicationContext applicationContext)
+        {
+            _db = applicationContext;
+            _dbSet = _db.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll(CancellationToken token)
+        {
+            return await _dbSet.AsNoTracking().ToListAsync(token);
+        }
+
+        public async Task<TEntity> Get(int id, CancellationToken token)
+        {
+            return await _dbSet.FindAsync(new object[] {id},token);
+        }
+
+        public async Task<TEntity> Create(TEntity tEntity , CancellationToken token)
+        {
+            _dbSet.AddAsync(tEntity, token);
+
+            _db.SaveChangesAsync(token);
+
+            return tEntity;
+        }
+
+        public async Task<TEntity> Update(TEntity tEntity , CancellationToken token)
+        {
+            _dbSet.Update(tEntity);
+
+            _db.SaveChangesAsync(token);
+
+            return tEntity;
+        }
+
+        public async Task Delete(int id, CancellationToken token)
+        {
+            var tEntity = _dbSet.Find(id);
+
+            _dbSet.Remove(tEntity);
+
+            await _db.SaveChangesAsync(token);
+        }
+    }
+}
