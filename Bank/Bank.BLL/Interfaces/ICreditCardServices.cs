961e9fd8e33dfef1551c310b48adbae86e574bbc
Fanar1n <95366750+Fanar1n@users.noreply.github.com>
2022-07-12 18:07:00 +0300

    Revert "Feature/add generic service (#22)" (#23)
    This reverts commit 7d6c5cfcbca6c260e7ec52838db90aac22d0d516.

--- Bank/Bank.BLL/Interfaces/ICreditCardServices.cs
+++ Bank/Bank.BLL/Interfaces/ICreditCardServices.cs
@@ -2,7 +2,12 @@
 
 namespace Bank.BLL.Interfaces
 {
-    public interface ICreditCardServices : IGenericService<CreditCard>
+    public interface ICreditCardServices
     {
+        Task<IEnumerable<CreditCard>> GetAll(CancellationToken token);
+        Task<CreditCard> Get(int id, CancellationToken token);
+        Task<CreditCard> Create(CreditCard item, CancellationToken token);
+        Task<CreditCard> Update(CreditCard item, CancellationToken token);
+        Task Delete(int id, CancellationToken token);
     }
 }
--- Bank/Bank/Validator/CreditCardValidator.cs
+++ Bank/Bank/Validator/CreditCardValidator.cs
@@ -10,8 +10,8 @@ namespace Bank.Validator
             RuleFor(x => x.Id).NotNull();
             RuleFor(x => x.CardNumber.ToString()).Length(4, 16);
             RuleFor(x => x.CVV.ToString()).Length(3);
-            RuleFor(x => x.OwnerFirstName).Length(1, 30);
-            RuleFor(x => x.OwnerSecondName).Length(1, 30);
+            RuleFor(x => x.OwnerFirstName).Length(1,30);
+            RuleFor(x => x.OwnerSecondName).Length(1,30);
         }
     }
 }
