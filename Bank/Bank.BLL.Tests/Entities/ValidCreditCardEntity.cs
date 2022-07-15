36a0d496bbd532d87d8945e3ecce4e67a9e4e431
Fanar1n <95366750+Fanar1n@users.noreply.github.com>
2022-07-15 14:36:13 +0300

    edit bll unittests (#32)

--- Bank/Bank.BLL.Tests/Entities/ValidCreditCardEntity.cs
+++ Bank/Bank.BLL.Tests/Entities/ValidCreditCardEntity.cs
@@ -6,10 +6,29 @@ namespace Bank.BLL.Tests.Entity
     {
         public static IEnumerable<CreditCardEntity> CreditCardEntityList = new List<CreditCardEntity>()
         {
-            new() { Id = 1, CardNumber = 77777, CVV = 777, OwnerFirstName = "Maksim", OwnerSecondName = "Shamigov" }
+            new() { Id = 1,
+                CardNumber = 77777,
+                CVV = 777, ClientId = 1,
+                Client = new()
+                {
+                    FirstName = "Vlad",
+                    SecondName = "Zagorsky",
+                    PhoneNumber = "+375293710904"
+                }}
         };
 
         public static CreditCardEntity CreditCardEntity = new()
-        { Id = 1, CardNumber = 77777, CVV = 777, OwnerFirstName = "Maksim", OwnerSecondName = "Shamigov" };
+        {
+            Id = 1,
+            CardNumber = 77777,
+            CVV = 777,
+            ClientId = 1,
+            Client = new()
+            {
+                FirstName = "Vlad",
+                SecondName = "Zagorsky",
+                PhoneNumber = "+375293710904"
+            }
+        };
     }
 }
