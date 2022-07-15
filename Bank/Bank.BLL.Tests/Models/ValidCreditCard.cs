88c7605542fafc951c33a9331122f87cd8af946a
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-15 12:26:59 +0300

    edit bll unittests

--- Bank/Bank.BLL.Tests/Models/ValidCreditCard.cs
+++ Bank/Bank.BLL.Tests/Models/ValidCreditCard.cs
@@ -6,10 +6,30 @@ namespace Bank.BLL.Tests.Models
     {
         public static IEnumerable<CreditCard> CreditCardList = new List<CreditCard>()
         {
-            new() { Id = 1, CardNumber = 77777, CVV = 777, OwnerFirstName = "Maksim", OwnerSecondName = "Shamigov" }
+            new() { Id = 1,
+                CardNumber = 77777,
+                CVV = 777,
+                ClientId = 1,
+                Client = new()
+                {
+                    FirstName = "Vlad",
+                    SecondName = "Zagorsky",
+                    PhoneNumber = "+375293710904"
+                }}
         };
 
         public static CreditCard CreditCard = new()
-        { Id = 1, CardNumber = 77777, CVV = 777, OwnerFirstName = "Maksim", OwnerSecondName = "Shamigov" };
+        {
+            Id = 1,
+            CardNumber = 77777,
+            CVV = 777,
+            ClientId = 1,
+            Client = new()
+            {
+                FirstName = "Vlad",
+                SecondName = "Zagorsky",
+                PhoneNumber = "+375293710904"
+            }
+        };
     }
 }
