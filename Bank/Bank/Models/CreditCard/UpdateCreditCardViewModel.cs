0e13689584d2a477b7529166e850f1fe3ec24b52
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-18 10:43:24 +0300

    add new model for update

---
+++ Bank/Bank/Models/CreditCard/UpdateCreditCardViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Bank.Models.CreditCard
+{
+    public class UpdateCreditCardViewModel
+    {
+        public int Id { get; set; }
+        public int CardNumber { get; set; }
+        public int CVV { get; set; }
+        public int ClientId { get; set; }
+    }
+}
